982e0e401c75d31f2316b217446e2ee966dc80de
Nathalia Robbins-Cherry <nchomeldoe@gmail.com>
2022-06-17 14:48:51 +0100

    passed signOut and user to NavBar from App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,24 +3,65 @@ import { Amplify } from "aws-amplify";
 import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 
+import React from "react";
+import { Router } from "@reach/router";
+
+import NavBar from "./components/NavBar/NavBar";
+import HomePage from "./components/HomePage/HomePage";
+import StudentsListView from "./components/StudentsListView/StudentsListView";
+import StudentView from "./components/StudentView/StudentView";
+import NewStudent from "./components/NewStudent/NewStudent";
+import NotificationProvider from "./context/NotificationProvider";
+import SubjectsListView from "./components/SubjectsListView/SubjectsListView";
+import ServiceProvider from "./context/ServiceProvider";
+
 Amplify.configure({
   region: "eu-west-2",
   userPoolId: "eu-west-2_tTOU6Scc7",
   userPoolWebClientId: "52s6fjmrntvjnf16013bkei8pa",
 });
 
-export default function App() {
+const App = () => {
   return (
-    <Authenticator hideSignUp={true}>
-      {({ signOut, user }) => (
-        <main>
-          <h1>Hello {user.username}</h1>
-          <button onClick={signOut}>Sign out</button>
-        </main>
-      )}
-    </Authenticator>
+    <>
+      <Authenticator hideSignUp={true}>
+        {({ signOut, user }) => (
+          <NotificationProvider>
+            <ServiceProvider>
+              <NavBar signOut={signOut} user={user} />
+              <Router>
+                <HomePage path="/" />
+                <StudentsListView path="/studentList" />
+                <SubjectsListView path="/subjectList" />
+                <StudentView path="/student/:studentId" />
+                <NewStudent path="/newStudent" />
+              </Router>
+            </ServiceProvider>
+          </NotificationProvider>
+          //         <main>
+          //           <h1>Hello {user.username}</h1>
+          //           <button onClick={signOut}>Sign out</button>
+          //         </main>
+        )}
+      </Authenticator>
+    </>
   );
-}
+};
+
+export default App;
+
+// export default function App() {
+//   return (
+//     <Authenticator hideSignUp={true}>
+//       {({ signOut, user }) => (
+//         <main>
+//           <h1>Hello {user.username}</h1>
+//           <button onClick={signOut}>Sign out</button>
+//         </main>
+//       )}
+//     </Authenticator>
+//   );
+// }
 
 // import React from "react";
 // import { Router } from "@reach/router";
--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,15 +1,33 @@
 import React from "react";
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
 import { navigate } from "@reach/router";
 
-const NavBar = () => {
+const NavBar = ({ signOut, user }) => {
+  const { username } = user;
   return (
     <AppBar sx={{ backgroundColor: "black" }}>
-      <Toolbar>
+      <Toolbar
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
         <div style={{ cursor: "pointer" }}>
           <Typography variant="h4" onClick={() => navigate(`/`)}>
             Nat's University
           </Typography>
+          <Typography>{username}</Typography>
+        </div>
+        <div>
+          <Button
+            variant="outlined"
+            color="error"
+            sx={{ color: "white", mr: 1 }}
+            onClick={signOut}
+          >
+            Sign out
+          </Button>
         </div>
       </Toolbar>
     </AppBar>
