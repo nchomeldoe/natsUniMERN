a5bb33c147d6e50d1a5ed4c0621ee53be28da889
Nathalia Robbins-Cherry <nchomeldoe@gmail.com>
2022-06-18 18:08:32 +0100

    removed localhost from urls

--- index.js
+++ index.js
@@ -6,29 +6,32 @@ const cors = require("cors");
 
 const studentsRouter = require("./routes/students.js");
 const subjectsRouter = require("./routes/subjects.js");
-console.log("****************", process.env);
+
+const { DB_URI, PORT, NODE_ENV } = process.env;
 const app = express();
-const port = process.env.PORT || 4000;
+const THIS_PORT = PORT || 4000;
 
 //Connect to mongodb
-const { DB_URI } = process.env;
-console.log("DB_URI", DB_URI);
 
 mongoose
   .connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB has been connected"))
   .catch((err) => console.log("mongo err:", err));
 
+// app
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/students", studentsRouter);
 app.use("/api/subjects", subjectsRouter);
-app.use(express.static(path.join(__dirname, "client", "build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}.`);
+if (NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client", "build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.listen(THIS_PORT, () => {
+  console.log(`Server is running on port ${THIS_PORT}.`);
 });
