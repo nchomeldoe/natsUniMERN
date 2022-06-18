f447a199792ac03210ab7edd91cfce3bad4c5f95
Nathalia Robbins-Cherry <nchomeldoe@gmail.com>
2022-06-18 15:54:30 +0100

    amended root index.js

--- index.js
+++ index.js
@@ -12,20 +12,21 @@ const port = process.env.PORT || 4000;
 //Connect to mongodb
 const { DB_URI } = process.env;
 
-mongoose
-  .connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((result) => {
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-    });
-    app.listen(port, () => {
-      console.log(`Server is running on port ${port}.`);
-    });
-  })
-  .catch((err) => console.error(err));
+// mongoose
+//   .connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then((result) => {
+//     app.get("*", (req, res) => {
+//       res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+//     });
+//     app.listen(port, () => {
+//       console.log(`Server is running on port ${port}.`);
+//     });
+//   })
+//   .catch((err) => console.error(err));
+
+mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(express.json());
-
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PATCH");
@@ -38,11 +39,17 @@ app.use((req, res, next) => {
 
 app.use("/api/students", studentsRouter);
 app.use("/api/subjects", subjectsRouter);
-
 app.use(express.static(path.join(__dirname, "client", "build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
-app.get("/api", (req, res) => res.send("Welcome to my API!"));
+// app.get("/api", (req, res) => res.send("Welcome to my API!"));
 
-app.get("*", (req, res) =>
-  res.status(404).send("There is no content at this route"),
-);
+// app.get("*", (req, res) =>
+//   res.status(404).send("There is no content at this route"),
+// );
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}.`);
+});
